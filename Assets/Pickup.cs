98400acd5beb640e9c3b7c5f5c1835a4f5381f32
MathalasQMUL <c.c.m.chow@se19.qmul.ac.uk>
2021-11-08 22:01:17 +0000

    Created script 'Pickup'
    Prototype of script to allow player to pick up object

---
+++ Assets/Pickup.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public float pickUpRange = 5;
+    public float moveForce = 250;
+    public Transform holdParent;
+
+    private GameObject heldObj;
+
+    void update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObj != null)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+                {
+                    PickupObject(hit.transform.gameObject);
+                }
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+
+
+
+        if (heldObj != null)
+        {
+            MoveObject(); 
+        }
+    }
+
+    void MoveObject()
+    {
+        if (Vector3.Distance(heldObj.transform.position, holdParent.position) > 0.1f)
+        {
+            Vector3 moveDirection = (holdParent.position - heldObj.transform.position);
+            heldObj.GetComponent<Rigidbody>().AddForce(moveDirection * moveForce);
+        }
+    }
+
+    void PickupObject(GameObject pickObj)
+    {
+        if (pickObj.GetComponent<Rigidbody>()) 
+        {
+            Rigidbody objRig = pickObj.GetComponent<Rigidbody>();
+            objRig.useGravity = false;
+            objRig.drag = 10;
+
+            objRig.transform.parent = holdParent;
+            heldObj = pickObj;
+        }
+    }
+
+    void DropObject()
+    {
+        Rigidbody heldRig = heldObj.GetComponent<Rigidbody>();
+        heldRig.useGravity = true;
+        heldRig.drag = 1;
+
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+}
