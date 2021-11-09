69c59fe28aa305f973de5512c63735127d1c7e98
Amir Saeidi <60146162+StudentAcc@users.noreply.github.com>
2021-11-09 19:28:15 +0000

    added gun flare

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class GunScript : MonoBehaviour
+public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
 
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
 
     // Update is called once per frame
     void Update()
@@ -18,10 +19,18 @@ public class GunScript : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
+
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
---
+++ Assets/Target.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
