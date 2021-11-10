9a50e8dc8c8441525753773b93f8b9cb4db6a885
MathalasQMUL <c.c.m.chow@se19.qmul.ac.uk>
2021-11-10 19:22:41 +0000

    Implemented 'Quit' button function
    Players can now quit the game when it ends.
    Renamed 'LoseScreen' script to 'MenuButtons'

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoseScreen : MonoBehaviour
+public class MenuButtons : MonoBehaviour
 {
     public void RestartGame()
     {
         SceneManager.LoadScene("Prototype");
     }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+
 }
