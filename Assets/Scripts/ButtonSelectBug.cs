0c5dab5fb3fd2ff9b9ffa69fb15ef8e6b51f39b0
Antonio-Alexandru Caragheorghe <a.caragheorghe@se19.qmul.ac.uk>
2021-11-12 06:26:23 +0000

    Fixed a bug in intro screen

--- Assets/Scripts/ButtonSelectBug.cs
+++ Assets/Scripts/ButtonSelectBug.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ButtonSelectBug : MonoBehaviour
 {
     public Button primaryButton;
-    // Start is called before the first frame update
+    
     void Start()
     {
         primaryButton.Select();
