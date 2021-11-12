8a31340208c6d2311deb1c4856b27a56adf70d8e
MathalasQMUL <c.c.m.chow@se19.qmul.ac.uk>
2021-11-12 03:06:50 +0000

    New change

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -9,7 +9,9 @@ public class CountDown : MonoBehaviour
 
     [SerializeField] Image timeImage;
     [SerializeField] Text timeText;
-    [SerializeField] float duration, currentTime;
+    [SerializeField] float duration;
+    public float currentTime;
+    [SerializeField] float decreaseByShooting;
 
 
     // Start is called before the first frame update
@@ -28,6 +30,7 @@ public class CountDown : MonoBehaviour
             timeText.text = currentTime.ToString();
             yield return new WaitForSeconds(1f);
             currentTime--;
+
             if (currentTime <= 20)
             {
                 timeImage.GetComponent<Image>().color = new Color32(255, 0, 0, 225);
@@ -36,6 +39,11 @@ public class CountDown : MonoBehaviour
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("LoseScreen");
+    }
+
+    public void onShootDecreaseOxygen()
+    {
+        currentTime -= decreaseByShooting;
     }
 
     // Update is called once per frame
