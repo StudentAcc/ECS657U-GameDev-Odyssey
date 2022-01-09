4afbd28676937a0ddf4fadf84ae4506006b49b40
MathalasQMUL <c.c.m.chow@se19.qmul.ac.uk>
2022-01-09 05:13:59 +0000

    Attempting to get the laser to do damage via raycast hit

--- Assets/VolumetricLines/BuiltInRenderPipeline/ExampleScenes/ExampleScripts/ShotBehavior.cs
+++ Assets/VolumetricLines/BuiltInRenderPipeline/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -7,6 +7,13 @@ public class ShotBehavior : MonoBehaviour
     public GameObject collisionExplosion;
     public float speed;
 
+    GunCombat combat;
+    RaycastHit hit;
+
+    void Start()
+    {
+        combat = GetComponent<GunCombat>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,6 +26,23 @@ public class ShotBehavior : MonoBehaviour
             if (transform.position == m_target)
             {
                 explode();
+                Ray ray = new Ray(transform.position, Vector3.up);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log("Clicked on " + hit.transform.name);
+                }
+                else
+                {
+                    Debug.Log("Nothing hit");
+                }
+
+                CharacterStats targetStats = hit.transform.GetComponent<CharacterStats>();
+
+                if (targetStats != null)
+                {
+                    combat.Attack(targetStats);
+                }
                 return;
             }
             transform.position = Vector3.MoveTowards(transform.position, m_target, step);
